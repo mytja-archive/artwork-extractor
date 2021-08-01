68b3a317edb3b8f9d6cd25db656a170b02f1f461
alexmercerind <saini123hitesh@gmail.com>
2021-08-02 01:15:45 +0530

    Added support for embedded artworks

--- artwork_extractor.cpp
+++ artwork_extractor.cpp
@@ -12,6 +12,17 @@
 #include "include/base64.hpp"
 #include "include/string.hpp"
 
+#define RM(x) \
+this->size -= (x); \
+this->data += (x); \
+
+
+static inline uint32_t U32_AT(const void* data) {
+    uint32_t x;
+    memcpy(&x, data, sizeof(x));
+    return __builtin_bswap32(x);
+}
+
 using namespace MediaInfoDLL;
 
 
@@ -25,23 +36,55 @@ public:
     std::string type = "";
     std::string description = "";
     std::string artwork = "";
+    uint8_t* data = nullptr;
+    size_t size = 0;
     
     ArtworkExtractor(std::string media, std::string output, bool followMime = false): media(media), output(output), followMime(followMime) {
         this->Option("Cover_Data", "base64");
         this->Open(this->media);
         this->isArtworkPresent = this->Get(Stream_General, 0, "Cover") == "Yes";
-        this->mime = this->Get(Stream_General, 0, "Cover_Mime");
-        this->type = this->Get(Stream_General, 0, "Cover_Type");
-        this->description = this->Get(Stream_General, 0, "Cover_Description");
         this->artwork = this->Get(Stream_General, 0, "Cover_Data");
+        std::vector<uint8_t> artwork = base64_decode(this->artwork);
+        this->data = new uint8_t[artwork.size()];
+        memcpy(this->data, artwork.data(), artwork.size() * sizeof(uint8_t));
+        this->size = artwork.size();
+        if (
+            Strings::split(Strings::toUpperCase(this->media), ".").back() == "OGG" ||
+            Strings::split(Strings::toUpperCase(this->media), ".").back() == "FLAC"
+        ) {
+            size_t header = 0;
+            uint32_t length;
+            this->type = std::to_string(U32_AT(this->data));
+            RM(4);
+            length = U32_AT(this->data);
+            header += length;
+            RM(4);
+            this->mime = strndup((const char*)this->data, length);
+            RM(length);
+            length = U32_AT(this->data);
+            header += length;
+            RM(4);
+            this->description = strndup((const char*)this->data, length);
+            RM(length);
+            RM(4 * 4);
+            length = U32_AT(this->data);
+            RM(4);
+            header += 32;
+            this->size = length;
+        }
+        else {
+            this->mime = this->Get(Stream_General, 0, "Cover_Mime");
+            this->type = this->Get(Stream_General, 0, "Cover_Type");
+            this->description = this->Get(Stream_General, 0, "Cover_Description");
+        }
     }
 
     void info() {
         if (this->isArtworkPresent) {
             std::cout << "Artwork found.\n";
-            std::cout << "Artwork mime       : " << this->mime << "\n";
-            std::cout << "Artwork type       : " << this->type << "\n";
-            std::cout << "Artwork description: " << this->description << "\n";   
+            std::cout << "Mime        : " << this->mime << "\n";
+            std::cout << "Type        : " << this->type << "\n";
+            std::cout << "Description : " << this->description << "\n";   
         }
         else {
             std::cout << "No artwork found.\n";
@@ -53,13 +96,15 @@ public:
             if (this->followMime) {
                 if (this->mime == "image/png") output += ".png";
                 if (Strings::startsWith(this->mime, "image/jp")) output += ".jpg";
-                else std::cout << "No mime found. Using .jpg as extension.\n";
+                else {
+                    output += ".jpg";
+                    std::cout << "No mime found. Using .jpg as extension.\n";
+                }
             }
-            std::vector<uint8_t> artwork = base64_decode(this->artwork);
             std::fstream file(this->output, std::ios::out | std::ios::binary);
             file.write(
-                (const char*)artwork.data(),
-                artwork.size()
+                (const char*)this->data,
+                this->size
             );
             file.flush();
             file.close();
@@ -77,7 +122,7 @@ int32_t main(int argc, const char** argv) {
     }
     if (args.size() < 3) {
         std::cout << "Invalid usage.\n";
-        std::cout << "Example:\n";
+        std::cout << "Example :\n";
         std::cout << args[0] << " media.mp3 output.png\n";
         return EXIT_FAILURE;
     }
--- include/string.hpp
+++ include/string.hpp
@@ -1,34 +1,32 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 #ifndef STRING_HEADER
 #define STRING_HEADER
 
-class String {
+class Strings {
 public:
-    inline static bool startsWith(std::string string, std::string subString) {
+    static inline bool startsWith(std::string string, std::string subString) {
         if (string.substr(0, subString.size()) == subString) return true;
         else return false;
     }
 
-    inline static std::vector<std::string> split(std::string string, std::string match) {
-        std::vector<std::string> result;
+    static inline std::vector<std::string> split(std::string string, std::string match) {
+        std::vector<std::string> result = {};
         int matchSize = match.size();
-        int lastIndex = 0;
+        int last = 0;
         for (int index = 0; index < string.size(); index++) {
-            if (index == string.size() - match.size() + 1) {
-                result.push_back(string.substr(lastIndex, string.size() - lastIndex));
-                break;
-            }
             if (string.substr(index, matchSize) == match) {
-                result.push_back(string.substr(lastIndex, index - lastIndex));
-                lastIndex = matchSize + index;
+                result.emplace_back(string.substr(last, index - last));
+                last = matchSize + index;
             }
         }
+        result.emplace_back(string.substr(last, string.size()));
         return result;
     }
 
-    inline static std::string replace(std::string string, std::string match, std::string replace) {
+    static inline std::string replace(std::string string, std::string match, std::string replace) {
         std::string result;
         int matchSize = match.size();
         int lastIndex = 0;
@@ -52,6 +50,11 @@ public:
             }
         }
         return result;
+    }
+
+    static inline std::string toUpperCase(std::string& string) {
+        std::transform(string.begin(), string.end(), string.begin(), ::toupper);
+        return string;
     }
 };
 
